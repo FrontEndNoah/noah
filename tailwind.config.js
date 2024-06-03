81fdc40dead6379b23573663d910e4989b74be0d
Noah Liang <super_mumoo@163.com>
2024-06-03 13:28:29 +0400

    inint tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+        success: 'var(--success-color)',
+        error: 'var(--error-color)',
+        warning: 'var(--warning-color)',
+      },
+      textColor: {
+        main: 'var(--font-color-main)',
+        sec: 'var(--font-color-second)',
+        acc: 'var(--font-color-accent)'
+      }
+    },
   },
   plugins: [],
 };
